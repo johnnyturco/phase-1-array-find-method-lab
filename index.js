15eca1fe2cdb54af3996b2ff18d39c099422eefa
Johnny <johnnyturco@me.com>
2022-11-02 18:10:18 -0600

    commit tree e40757335384684809b395392e086accd4c6f614

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function superbowlWin(record){
+  let blah = record.find(function(element){
+    if(element.result === "W"){
+      return true;
+    }
+  })
+  if(blah === undefined){
+    return undefined;
+  } else {
+    return blah.year;
+  }
+}
